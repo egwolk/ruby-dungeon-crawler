14a69d1ba69c92512940cb695e4269ffecbbd790
suxql <20226410@s.ubaguio.edu>
2026-05-10 22:29:53 +0800

    implement game loop to include room progression and improve player greeting format

--- main.rb
+++ main.rb
@@ -2,14 +2,26 @@ require_relative 'player'
 require_relative 'enemy'
 require_relative 'battle'
 
-
 puts "Welcome to the Dungeon!"
 print "Enter your name: "
 name = gets.chomp
 
 player = Player.new(name)
-puts "Hello. #{player.name}! You have #{player.hp} HP."
+puts "Hello, #{player.name}! You have #{player.hp} HP."
 puts "You enter the dungeon..."
 
-enemy = Enemy.random
-Battle.new(player, enemy).start
+room = 1
+
+loop do
+  puts "\n--- Room #{room} ---"
+
+  enemy = Enemy.random
+  Battle.new(player, enemy).start
+
+  break if player.hp <= 0
+
+  room += 1
+  puts "\nYou move deeper into the dungeon..."
+end
+
+puts "\nGame over! You made it through #{room - 1} room(s). Thanks for playing!"
