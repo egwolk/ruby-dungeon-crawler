16b07d90bea7ff76d41aa14a293acf5d57628a3b
suxql <20226410@s.ubaguio.edu>
2026-05-14 00:46:56 +0800

    inventory item interactions

--- battle.rb
+++ battle.rb
@@ -92,59 +92,106 @@ class Battle
 
     return if @player.inventory.items.empty?
 
-    puts "\nWhat would you like to do?"
-    puts "Enter item number to use, 'e' to equip, 'u' to unequip, 'd' to discard, or '0' to go back:"
-    choice = gets.chomp.downcase
+    puts "\nSelect item number or '0' to go back:"
+    choice = gets.chomp.to_i
 
-    if choice == "0"
+    if choice == 0
       return
-    elsif choice == "d"
-      discard_item
-    elsif choice == "e"
-      puts "Which item number to equip?"
-      num = gets.chomp.to_i
-      if num > 0 && num <= @player.inventory.items.size
-        @player.inventory.equip_item(num - 1, @player)
-      else
-        puts "Invalid selection."
-      end
-    elsif choice == "u"
-      puts "Which item number to unequip?"
-      num = gets.chomp.to_i
-      if num > 0 && num <= @player.inventory.items.size
-        @player.inventory.unequip_item(num - 1, @player)
-      else
-        puts "Invalid selection."
-      end
-    elsif choice.to_i > 0 && choice.to_i <= @player.inventory.items.size
-      item = @player.inventory.items[choice.to_i - 1]
-      use_item(item, choice.to_i - 1)
+    elsif choice > 0 && choice <= @player.inventory.items.size
+      item = @player.inventory.items[choice - 1]
+      show_item_menu(item, choice - 1)
     else
       puts "Invalid selection."
     end
   end
 
-  def discard_item
-    puts "\nWhich item do you want to discard? (Enter number):"
-    choice = gets.chomp.to_i
+  def show_item_menu(item, index)
+    equippable = [:weapon, :shield, :ring, :hat]
     
-    if choice > 0 && choice <= @player.inventory.items.size
-      item = @player.inventory.items[choice - 1]
-      puts "Are you sure you want to discard #{item.name}? (Y/N)"
-      confirm = gets.chomp.downcase
-      
-      if confirm == "y"
-        # If item is equipped, unequip first to adjust stats
-        if item.equipped
-          @player.inventory.unequip_item(choice - 1, @player)
-        end
-        @player.inventory.remove_item(choice - 1)
-        puts "You discarded the #{item.name}."
+    if equippable.include?(item.type)
+      handle_equippable_menu(item, index)
+    elsif item.type == :potion
+      handle_potion_menu(item, index)
+    else
+      handle_generic_menu(item, index)
+    end
+  end
+
+  def handle_equippable_menu(item, index)
+    puts "\nWhat would you like to do?"
+    equipped_status = item.equipped ? "unequip" : "equip"
+    puts "1. #{equipped_status.capitalize}"
+    puts "2. Discard"
+    puts "3. Go back"
+    
+    choice = gets.chomp
+    
+    case choice
+    when "1"
+      if item.equipped
+        @player.inventory.unequip_item(index, @player)
       else
-        puts "You decided to keep it."
+        @player.inventory.equip_item(index, @player)
       end
+    when "2"
+      discard_item_at(index)
+    when "3"
+      return
     else
       puts "Invalid selection."
+    end
+  end
+
+  def handle_potion_menu(item, index)
+    puts "\nWhat would you like to do?"
+    puts "1. Drink"
+    puts "2. Discard"
+    puts "3. Go back"
+    
+    choice = gets.chomp
+    
+    case choice
+    when "1"
+      use_item(item, index)
+    when "2"
+      discard_item_at(index)
+    when "3"
+      return
+    else
+      puts "Invalid selection."
+    end
+  end
+
+  def handle_generic_menu(item, index)
+    puts "\nWhat would you like to do?"
+    puts "1. Discard"
+    puts "2. Go back"
+    
+    choice = gets.chomp
+    
+    case choice
+    when "1"
+      discard_item_at(index)
+    when "2"
+      return
+    else
+      puts "Invalid selection."
+    end
+  end
+
+  def discard_item_at(index)
+    item = @player.inventory.items[index]
+    puts "Are you sure you want to discard #{item.name}? (Y/N)"
+    confirm = gets.chomp.downcase
+    
+    if confirm == "y"
+      if item.equipped
+        @player.inventory.unequip_item(index, @player)
+      end
+      @player.inventory.remove_item(index)
+      puts "You discarded the #{item.name}."
+    else
+      puts "You decided to keep it."
     end
   end
 
